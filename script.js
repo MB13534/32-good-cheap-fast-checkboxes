cf2aa59f4eadff6e8bf24f3850579bdae47e1c19
Michael <michael.barry.email@gmail.com>
2020-10-16 19:19:13 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const good = document.getElementById('good');
+const cheap = document.getElementById('good');
+const fast = document.getElementById('fast');
+const toggles = document.querySelectorAll('.toggle')
+
+toggles.forEach(toggle => {
+    toggle.addEventListener('change', (e) => {
+        doTheTrick(e.target);
+    })
+})
+
+
+function doTheTrick(theClickedOne) {
+    if(good.checked && cheap.checked && fast.checked){
+        if(good === theClickedOne) {
+            fast.checked = false
+        }
+
+        if (cheap === theClickedOne) {
+            good.checked = false
+        }
+
+        if(fast === theClickedOne) {
+            cheap.checked = false
+        }
+    }
+}
